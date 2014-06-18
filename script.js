1d7616d1e1e74341a2fdc3b62f3ab87151e9da51
Wayne Chi <waynchi@gmail.com>
2014-06-18 17:30:53 +0800

    Added Buy minion and looking at how to constantly update Gold

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 //Variables
+var gold = 0;
 var minionsKilled = 0;
+var minionsOwned = 0;
 
 function killMinion() {
 	minionsKilled += 1;
+	Gold += 1;
 	document.getElementById("MinionsKilledCount").innerHTML = minionsKilled;
+}
+
+function buyMinion() {
+	minionsOwned +=1;
+	Gold -= 10;
+	document.getElementById("MinionsOwned").innerHTML = minionsOwned;
+}
+	
+
+function updateGold() {
+    document.getElementById("Gold").innerHTML = gold;
+	alert ("Test");
 }
