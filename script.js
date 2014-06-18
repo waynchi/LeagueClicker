5d42bdd5c5a4ad2ffe2b6fc85f72b85ec0525dee
Wayne Chi <waynchi@gmail.com>
2014-06-18 17:38:20 +0800

    Gold can now be added and subtracted

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 //Variables
+var myVar = setInterval(function(){updateGold()},1000);
 var gold = 0;
 var minionsKilled = 0;
 var minionsOwned = 0;
 
 function killMinion() {
 	minionsKilled += 1;
-	Gold += 1;
+	gold += 1;
+	updateGold();
 	document.getElementById("MinionsKilledCount").innerHTML = minionsKilled;
 }
 
 function buyMinion() {
 	minionsOwned +=1;
-	Gold -= 10;
+	gold -= 10;
+	updateGold();
 	document.getElementById("MinionsOwned").innerHTML = minionsOwned;
 }
-	
 
 function updateGold() {
     document.getElementById("Gold").innerHTML = gold;
-	alert ("Test");
 }
