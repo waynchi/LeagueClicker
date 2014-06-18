d71dc3ac88666d8f3f59942a55715f4b9a70ffc6
Wayne Chi <waynchi@gmail.com>
2014-06-18 17:41:10 +0800

    Minions now generate gold per second

--- script.js
+++ script.js
@@ -7,17 +7,21 @@ var minionsOwned = 0;
 function killMinion() {
 	minionsKilled += 1;
 	gold += 1;
-	updateGold();
+	document.getElementById("Gold").innerHTML = gold;
 	document.getElementById("MinionsKilledCount").innerHTML = minionsKilled;
 }
 
 function buyMinion() {
-	minionsOwned +=1;
-	gold -= 10;
-	updateGold();
-	document.getElementById("MinionsOwned").innerHTML = minionsOwned;
+	if(gold >=10)
+	{
+		minionsOwned +=1;
+		gold -= 10;
+		document.getElementById("Gold").innerHTML = gold;
+		document.getElementById("MinionsOwned").innerHTML = minionsOwned;
+	}
 }
 
 function updateGold() {
+	gold += minionsOwned*0.1;
     document.getElementById("Gold").innerHTML = gold;
 }
