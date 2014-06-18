d60d4e5f7e389a8aaf5388cf408040d9e27198f6
Wayne Chi <waynchi@gmail.com>
2014-06-18 18:49:09 +0800

    Made the Buy minions section appear after a certain amount of gold

--- script.js
+++ script.js
@@ -4,11 +4,19 @@ var gold = 0;
 var minionsKilled = 0;
 var minionsOwned = 0;
 
+//Variables that see if Element has been created
+var buyMinionBlockTrue = false;
+
 function killMinion() {
 	minionsKilled += 1;
 	gold += 1;
 	document.getElementById("Gold").innerHTML = gold;
 	document.getElementById("MinionsKilledCount").innerHTML = minionsKilled;
+	if(minionsKilled >= 10 && !buyMinionBlockTrue)
+	{	
+		buyMinionBlockTrue = true;
+		showBuyMinion();
+	}
 }
 
 function buyMinion() {
@@ -17,8 +25,37 @@ function buyMinion() {
 		minionsOwned +=1;
 		gold -= 10;
 		document.getElementById("Gold").innerHTML = gold;
+		console.log(gold);
 		document.getElementById("MinionsOwned").innerHTML = minionsOwned;
+		console.log(minionsOwned);
 	}
+}
+
+//create element to Buy Minion
+function showBuyMinion() {
+	//creating Buttons and Text
+	var buyMinionBlockDiv = document.createElement("Div");
+	var buyMinionBtn = document.createElement("Button");
+	var buyMinionTxt = document.createTextNode("Buy Minion");
+	var buyMinionTextSpan = document.createElement("Span");
+	var minionOwnedTxt = document.createTextNode("Minions Owned: ");
+	var minionAmtSpan = document.createElement("Span");
+	var minionAmtTxt = document.createTextNode(minionsOwned);
+	//appending items together
+	buyMinionBtn.appendChild(buyMinionTxt);
+	buyMinionBlockDiv.appendChild(buyMinionBtn);
+	buyMinionTextSpan.appendChild(minionOwnedTxt);
+	minionAmtSpan.appendChild(minionAmtTxt);
+	buyMinionTextSpan.appendChild(minionAmtSpan);
+	buyMinionBlockDiv.appendChild(buyMinionTextSpan);
+	document.body.appendChild(buyMinionBlockDiv);
+	//setting ID
+	buyMinionBlockDiv.id = "buyMinionBlock";
+	minionAmtSpan.id = "MinionsOwned";
+	buyMinionBtn.id = "buyMinion";
+	//setting onclick
+	buyMinionBtn.onclick = function(){buyMinion()};
+	
 }
 
 function updateGold() {
